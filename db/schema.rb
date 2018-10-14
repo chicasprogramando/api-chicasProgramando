e19371490f781183963398996c9e8d24dd6d991d
Mar Calmon <mariana@yellowspot.com.ar>
2018-10-14 11:21:56 -0300

    Added the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_121023) do
+ActiveRecord::Schema.define(version: 2018_10_14_140656) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -36,6 +36,22 @@ ActiveRecord::Schema.define(version: 2018_10_01_121023) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.string "image"
+    t.string "role_name"
+    t.boolean "search_project"
+    t.string "senority"
+    t.string "skills"
+    t.text "about_me"
+    t.string "linkedin"
+    t.string "github"
+    t.string "twitter"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
