c23aaf3bd1ee8b43f759b268497c3f4ee899b399
Eleonora Lester <eleonora.lester@gmail.com>
2019-05-12 20:02:40 -0300

    feat: Adds create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, format: 'json' do
-      resources :users, only: [:index, :show] do
+      resources :users, only: [:index, :show, :create] do
         collection do
           get :me
         end
