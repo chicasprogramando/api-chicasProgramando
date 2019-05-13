9e588e74282637fc254e637cbdfd340d87441c9b
Eleonora Lester <eleonora.lester@gmail.com>
2019-05-13 09:41:11 -0300

    fix: projects_spec.rb was commented ouch

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -1,38 +1,38 @@
-# require "rails_helper"
+require "rails_helper"
 
-# describe "Projects API" do
+describe "Projects API" do
 
-#   def project_json(p, full: false)
-#     includes = {except: [:created_at, :updated_at]}
+  def project_json(p, full: false)
+    includes = {except: [:created_at, :updated_at]}
 
-#     if !full
-#       includes[:only] = [:id, :project_name, :created_by, :technologies]
-#     end
+    if !full
+      includes[:only] = [:id, :project_name, :created_by, :technologies]
+    end
 
-#     p.as_json(includes).stringify_keys
-#   end
+    p.as_json(includes).stringify_keys
+  end
 
-#   context "#index" do
-#     let!(:projects) { create_list :project, 3 }
-#     let(:expected_projects) { projects.map{|p| project_json(p) }}
+  context "#index" do
+    let!(:projects) { create_list :project, 3 }
+    let(:expected_projects) { projects.map{|p| project_json(p) }}
 
-#     it "return projects" do
-#       get api_projects_path
+    it "return projects" do
+      get api_projects_path
 
-#       expect(json).to have_key('projects')
-#       expect(json['projects'].count).to eq 3
-#       expect(json['projects']).to match_array expected_projects
-#     end
-#   end
+      expect(json).to have_key('projects')
+      expect(json['projects'].count).to eq 3
+      expect(json['projects']).to match_array expected_projects
+    end
+  end
 
-#   context "#show" do
-#     let(:project) { create :project }
+  context "#show" do
+    let(:project) { create :project }
 
-#     it "return project" do
-#       get api_project_path(project)
+    it "return project" do
+      get api_project_path(project)
 
-#       expect(json).to have_key('project')
-#       expect(json['project']).to eq project_json(project, full: true)
-#     end
-#   end
-# end
+      expect(json).to have_key('project')
+      expect(json['project']).to eq project_json(project, full: true)
+    end
+  end
+end
