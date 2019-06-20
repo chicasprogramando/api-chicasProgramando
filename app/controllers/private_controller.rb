dc5bf629c116fe44186152a6be297f5e7edb9850
elstr <eleonora.lester@gmail.com>
2019-06-20 13:42:54 -0300

    borra Scope y ruta private de test

--- app/controllers/private_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-class PrivateController < ApplicationController
-    include Secured
-  
-    def private
-      render json: {message: 'Funciona'}, status: :ok
-    end
-end
